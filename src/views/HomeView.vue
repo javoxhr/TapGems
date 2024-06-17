5dc2a95de3d1964852f33e7981c7702294a97b97
javoxhr <javohiruzum@gmail.com>
2024-06-17 13:51:47 +0500

    new pag

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,17 @@
       <h1>Совсем скоро</h1>
       <h1 class="gems">💎</h1>
     </div>
-    <button>click</button>
+
+    <header>
+      <div class="container">
+         <div class="header-wrapper">
+            <h1 class="header-wrapper__title">TapGems</h1>
+            <router-link to="/">
+              <span class="logo">💎</span>
+            </router-link>
+         </div>
+       </div>
+    </header>
   </main>
 </template>
 
