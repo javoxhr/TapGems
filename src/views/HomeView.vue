b22fc37ceb9bcb75284a9cb40a6fe0c6d3ba39e9
javoxhr <javohiruzum@gmail.com>
2024-08-03 12:53:00 +0500

    NEW

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="overlay"></div>
-  <div class="loader" :style="{'display': show == true ? 'none' : 'none'}">
+  <div class="loader" :style="{'display': show ? 'none' : 'none'}">
     <span class="loading"></span>
     <span class="loading"></span>
     <span class="loading"></span>
@@ -15,7 +15,7 @@
     <top>
       <div class="container">
         <div class="header-wrapper">
-          <h1 class="header-wrapper__balans" :class="{'after-balans': show == true}"  >
+          <h1 class="header-wrapper__balans" :class="{'after-balans': show}">
             <span class="total">{{ total }}</span>
             <span class="zap">.</span>
             <span>{{ centerCoin }}</span>
@@ -44,24 +44,14 @@
     <div class="limit">
       <div class="limit-wrap">
         <button class="energiya">
-          <!-- <img src="../assets/images/energiy.svg" alt="" /> -->
-         <!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
-         <svg enable-background="new 0 0 512 512" height="512px" id="Слой_1" version="1.1" viewBox="0 0 512 512" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><g><g><path clip-rule="evenodd" d="M336.949,83.675c0.35,0.002,0.697,0.002,1.048,0h1.048    
-            c0.455,0.011,0.9,0.029,1.339,0.059c7.456,0.473,13.878,3.421,19.262,8.844c5.953,5.948,8.94,13.125,8.963,21.53v332.837    
-              c-0.005,0.471-0.025,0.937-0.059,1.396c-0.321,7.806-3.287,14.518-8.904,20.134c-5.577,5.644-12.249,8.633-20.019,8.96    
-              c-0.443,0.036-0.889,0.055-1.339,0.06H173.654c-0.412-0.005-0.818-0.023-1.222-0.06c-7.817-0.315-14.528-3.303-20.136-8.96     
-              c-5.936-5.946-8.903-13.124-8.904-21.529V114.108c0.001-8.405,2.969-15.582,8.904-21.53c5.889-5.935,13.008-8.901,21.357-8.903    
-              v0.059c86.639-0.03,140.374-0.05,161.201-0.059C335.554,83.68,336.251,83.68,336.949,83.675z M225.972,408.018v20.714h60.116  
-             v-20.714H225.972z M206.011,297.808h39.514l-17.982,62.262c-0.359,1.248-0.282,2.471,0.233,3.667     
-              c0.512,1.186,1.346,2.079,2.503,2.677c1.149,0.609,2.352,0.784,3.607,0.523c1.27-0.269,2.317-0.909,3.143-1.921l73.267-91.472   
-             c0.869-1.116,1.237-2.378,1.107-3.782c-0.091-1.423-0.652-2.625-1.689-3.607c-1.057-0.993-2.298-1.498-3.724-1.514h-39.513     
-             l17.981-62.202c0.358-1.289,0.281-2.548-0.232-3.782c-0.514-1.168-1.346-2.041-2.503-2.618    
-            c-1.171-0.594-2.394-0.748-3.667-0.467c-1.291,0.269-2.356,0.909-3.2,1.921l-73.15,91.472c-0.907,1.096-1.315,2.337-1.224,3.724  
-            c0.129,1.44,0.73,2.663,1.806,3.666C203.343,297.326,204.585,297.811,206.011,297.808z M266.358,132.496V112.77H245.7v19.727  
-            h-19.729v20.714H245.7v19.728h20.658V153.21h19.729v-20.714H266.358z"
-             :fill="svgColor" fill-rule="evenodd"/></g><g><rect clip-rule="#fff"
-           :fill="svgColor" fill-rule="evenodd" height="32px" width="106px"
-            x="202.984" y="33.808"/></g></g></g></svg>
+          <svg width="32px" height="32px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+            <g>
+              <path 
+                d="M336.949,83.675c0.35,0.002,0.697,0.002,1.048,0h1.048c0.455,0.011,0.9,0.029,1.339,0.059c7.456,0.473,13.878,3.421,19.262,8.844c5.953,5.948,8.94,13.125,8.963,21.53v332.837c-0.005,0.471-0.025,0.937-0.059,1.396c-0.321,7.806-3.287,14.518-8.904,20.134c-5.577,5.644-12.249,8.633-20.019,8.96c-0.443,0.036-0.889,0.055-1.339,0.06H173.654c-0.412-0.005-0.818-0.023-1.222-0.06c-7.817-0.315-14.528-3.303-20.136-8.96c-5.936-5.946-8.903-13.124-8.904-21.529V114.108c0.001-8.405,2.969-15.582,8.904-21.53c5.889-5.935,13.008-8.901,21.357-8.903v0.059c86.639-0.03,140.374-0.05,161.201-0.059C335.554,83.68,336.251,83.68,336.949,83.675z M225.972,408.018v20.714h60.116v-20.714H225.972z M206.011,297.808h39.514l-17.982,62.262c-0.359,1.248-0.282,2.471,0.233,3.667c0.512,1.186,1.346,2.079,2.503,2.677c1.149,0.609,2.352,0.784,3.607,0.523c1.27-0.269,2.317-0.909,3.143-1.921l73.267-91.472c0.869-1.116,1.237-2.378,1.107-3.782c-0.091-1.423-0.652-2.625-1.689-3.607c-1.057-0.993-2.298-1.498-3.724-1.514h-39.513l17.981-62.202c0.358-1.289,0.281-2.548-0.232-3.782c-0.514-1.168-1.346-2.041-2.503-2.618c-1.171-0.594-2.394-0.748-3.667-0.467c-1.291,0.269-2.356,0.909-3.2,1.921l-73.15,91.472c-0.907,1.096-1.315,2.337-1.224,3.724c0.129,1.44,0.73,2.663,1.806,3.666C203.343,297.326,204.585,297.811,206.011,297.808z M266.358,132.496V112.77H245.7v19.727h-19.729v20.714H245.7v19.728h20.658V153.21h19.729v-20.714H266.358z" 
+                :fill="svgColor" 
+              />
+            </g>
+          </svg>
         </button>
         <h1 class="limit-text">{{ calc }}</h1>
         <h3 class="slash">/</h3>
@@ -75,72 +65,76 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-const audioElement = ref(null)
+import { ref, onMounted } from 'vue';
+
+const audioElement = ref(null);
 
 const playAudio = () => {
-  if(audioElement.value) {
-    audioElement.value.play()
+  if (audioElement.value) {
+    const audio = audioElement.value;
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play().catch(error => {
+      console.error("Ошибка воспроизведения звука:", error);
+    });
   }
-}
+};
+
 
 const total = ref(0);
 const coin = ref(0);
 const centerCoin = ref(0);
-
 const calc = ref(300);
+const show = ref(false);
+const svgColor = ref('#9acd32');
 
-let show = ref(false)
+const clickFunc = () => {
+  addTotal();
+  showFunc();
+  vibrate();
+};
 
-const svgColor = ref('#9acd32')
+const showFunc = () => {
+  show.value = !show.value;
+};
 
-function clickFunc() {
-  addTotal()
-  showFunc()
-  vibrate()
-}
-
-
-function showFunc() {
-  show.value = !show.value
-}
-
-function vibrate() {
-  if(vibrate.navigator) {
-    navigator.vibrate(200)
-    console.log('hello world')
+const vibrate = () => {
+  if (navigator.vibrate) {
+    navigator.vibrate(200);
+    console.log('hello world');
   } else {
-    console.log('Вибрация не поддерживается этим устройством.')
+    console.log('Вибрация не поддерживается этим устройством.');
   }
-}
+};
 
-function addTotal() {
-  if(calc.value <= 50) {
-    svgColor.value = 'Brown'
+const addTotal = () => {
+  if (calc.value <= 50) {
+    svgColor.value = 'Brown';
   } else {
-    svgColor.value = '#9acd32'
+    svgColor.value = '#9acd32';
   }
+
   if (calc.value >= 5) {
     total.value += 1;
-    calc.value -= 5
-    playAudio()
+    calc.value -= 5;
+    playAudio();
   } else {
-     console.log("hello");
-     total.value += 0;
-   }
+    console.log("hello");
+  }
+
   localStorage.setItem("count", total.value);
-}
+};
+
 onMounted(() => {
-  total.value = localStorage.getItem("count")
-    ? Number(localStorage.getItem("count"))
-    : 0;
+  total.value = localStorage.getItem("count") ? Number(localStorage.getItem("count")) : 0;
+
   setInterval(() => {
-    if (calc.value !== 300 || calc.value < 300) {
-       calc.value += 5;
+    if (calc.value < 300) {
+      calc.value += 5;
     }
 
-    if(calc.value >= 50) {
-      svgColor.value = '#9acd32'
+    if (calc.value >= 50) {
+      svgColor.value = '#9acd32';
     }
   }, 3000);
 });
